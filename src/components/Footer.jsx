0e9944db7e4d8a6da3d6d5273ac216a424aeda4e
1stmuse <olysegs@gmail.com>
2020-05-20 22:11:38 +0100

    added some footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,46 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        
+        <div className='footer-content'>
+          <div className='footer-grid'>
+            <div>
+              <ul>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+                <li>Services</li>
+              </ul>
+            </div>
+          </div>
+          <div className='footer-wd'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              sicing elit. Necessitatibus laborum quibusdam 
+              laboriosam blanditiis odit illum.
+            </p>
+            <div className='social'>
+              <i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+              <i className='fa fa-twitter fa-2x' aria-hidden></i>
+              <i className='fa fa-instagram fa-2x' aria-hidden></i>
+            </div>
+          </div>
+        </div>
       </footer>
     )
   }
